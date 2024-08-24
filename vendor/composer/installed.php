57d20f0fbdc306e8c77f2cfbd763b9c52874cc00
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-08-24 09:27:08 +0700

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ae2edcb340e335c4707c2112c4723ed71dd6b79d',
+        'name' => 'osykunchen/belajar-php-composer-hello',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'osykunchen/belajar-php-composer-hello' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ae2edcb340e335c4707c2112c4723ed71dd6b79d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
